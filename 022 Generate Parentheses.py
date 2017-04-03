cb1d035619b63d3a70317cfec0118fc170cf149a
Yuan Yao <pengpeng9045@gmail.com>
2017-04-02 23:14:22 -0400

    Create 022 Generate Parentheses.py

---
+++ 022 Generate Parentheses.py
@@ -0,0 +1,19 @@
+__author__ = 'yyao'
+
+### https://soulmachine.gitbooks.io/algorithm-essentials/content/java/dfs/generate-parentheses.html
+
+def generateParenthesis(n):
+    result = []
+    generate(n,n,'',result)
+    return result
+
+def generate(left,right,str,result):
+    if left == 0 and right == 0:
+        result.append(str)
+        return
+    if left >0:
+        generate(left-1,right,str+'(',result)
+    if right > left:
+        generate(left,right-1,str+')',result)
+
+print(generateParenthesis(3))
