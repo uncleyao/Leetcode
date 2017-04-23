a4ebb531dcd9a3094a6bf704c043513ec8552504
Yuan Yao <pengpeng9045@gmail.com>
2017-04-23 16:21:07 -0400

    Create 125 Valid Palindrome.py

---
+++ 125 Valid Palindrome.py
@@ -0,0 +1,20 @@
+__author__ = 'yyao'
+
+'''
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+For example,
+"A man, a plan, a canal: Panama" is a palindrome.
+"race a car" is not a palindrome.
+Note:
+Have you consider that the string might be empty? This is a good question to ask during an interview.
+For the purpose of this problem, we define empty string as valid palindrome.
+'''
+
+def isPalindrome(strin):
+    newstrin = [s for s in strin.lower() if s.isalnum()]
+    return newstrin == newstrin[::-1]
+
+
+print(isPalindrome("A man, a plan, a canal: Panama"))
+print(isPalindrome("race a car"))
+
