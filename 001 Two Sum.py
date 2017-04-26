08edbf5e02c65eaa291f3c3978b9d6e50804426e
Yuan Yao <pengpeng9045@gmail.com>
2017-04-26 01:16:44 -0400

    Update 001 Two Sum.py

--- 001 Two Sum.py
+++ 001 Two Sum.py
@@ -23,6 +23,18 @@ def sumtarget(sourcelist, num):
 
     return result_list
 
+## my solution
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i,n in enumerate(nums):
+            if target-n in nums[i+1:]:
+                return [i,nums[i+1:].index(target-n)+i+1]
+            
 s = sumtarget([1,2,7,4,5],6)
 
 for result in s:
