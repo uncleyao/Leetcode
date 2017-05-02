e1ba4ac6951fa7e71084b3fb4a44d4e325fd7f19
Yuan Yao <pengpeng9045@gmail.com>
2017-05-01 22:43:02 -0400

    Update 053 Maximum Subarray.py

--- 053 Maximum Subarray.py
+++ 053 Maximum Subarray.py
@@ -16,6 +16,14 @@ class Solution(object):
         
         current = nums[0]
         m= current
+        
+        ''' For every int, there are 2 situations:
+        1) add into the subarray
+        2) start with it
+        So if that one is less than 0, forget about former, or just add it
+        then each time we will a result, we should calculate max of them,
+        so do m = max(m,current) each loop
+        '''
         for i in range(1,len(nums)):
             if current < 0:
                 current = 0
