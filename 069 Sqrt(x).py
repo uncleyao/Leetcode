2b70ec48f5984fb1f91a170fa02b0c0d54b760dc
Yuan Yao <pengpeng9045@gmail.com>
2017-05-07 15:27:48 -0400

    Update 069 Sqrt(x).py

--- 069 Sqrt(x).py
+++ 069 Sqrt(x).py
@@ -4,13 +4,23 @@ __author__ = 'yyao'
 Implement int sqrt(int x).
 Compute and return the square root of x.
 '''
+"""
+So called Newton Method
+x: f(x) = 0
+approaching:
+xn+1 = xn - f(xn)/f`(xn)
+"""
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x<0:
+            return False
 
-def mySqrt(x):
-    newx = [i for i in range(x+1) if i*i <= x]
-    return newx[-1]
-
-print(mySqrt(0))
-print(mySqrt(1))
-print(mySqrt(4))
-print(mySqrt(82))
-print(mySqrt(100))
+        ans = x
+        eps = 0.0001
+        while abs(ans*ans)-x>eps:
+            ans = (ans+x/ans)/2
+        return ans
