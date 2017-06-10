3f8f28bb053ec1dabeba3a548d3cc92730504545
Yuan Yao <pengpeng9045@gmail.com>
2017-06-10 18:38:08 -0400

    Create 055 Jump Game.py

---
+++ 055 Jump Game.py
@@ -0,0 +1,29 @@
+"""
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+For example:
+A = [2,3,1,1,4], return true.
+
+A = [3,2,1,0,4], return false.
+
+Subscribe to see which companies asked this question.
+"""
+
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if not nums:
+            return False
+        maxReach = 0
+        for i in range(len(nums)):
+            if i <= maxReach:
+                maxReach = max(maxReach,i+nums[i])
+                
+        return maxReach >=len(nums)-1
