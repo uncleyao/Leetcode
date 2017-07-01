012688e50458b9ebd584a235ccff1991ea9fae66
Yuan Yao <pengpeng9045@gmail.com>
2017-07-01 17:37:15 -0400

    Create 200 Number of Islands.py

---
+++ 200 Number of Islands.py
@@ -0,0 +1,46 @@
+"""
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+
+11110
+11010
+11000
+00000
+Answer: 1
+
+Example 2:
+
+11000
+11000
+00100
+00011
+Answer: 3
+"""
+
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid:
+            return 0
+        m = len(grid)
+        n = len(grid[0])
+        result = 0
+        
+        def dfs(i,j):
+            if 0<=i<m and 0<=j<n and grid[i][j]=="1":
+                grid[i][j]=0
+                dfs(i-1,j)
+                dfs(i+1,j)
+                dfs(i,j-1)
+                dfs(i,j+1)
+                return 1
+            return 0
+        
+        for i in range(m):
+            for j in range(n):
+                result += dfs(i,j)
+        return result
