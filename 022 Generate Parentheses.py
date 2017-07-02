2a9551d28ec695ffb3530dcf1dbd4d9eda2b7a72
Yuan Yao <pengpeng9045@gmail.com>
2017-07-01 22:03:27 -0400

    Update 022 Generate Parentheses.py

--- 022 Generate Parentheses.py
+++ 022 Generate Parentheses.py
@@ -4,17 +4,17 @@ __author__ = 'yyao'
 
 def generateParenthesis(n):
     result = []
-    generate(n,n,'',result)
+    self.generate(n,n,'',result)
     return result
 
-def generate(left,right,str,result):
+def generate(self.left,right,str,result):
     if left == 0 and right == 0:
         result.append(str)
         return
     if left >0:
-        generate(left-1,right,str+'(',result)
+        self.generate(left-1,right,str+'(',result)
         ##this part is important, you can only do right > left, b/c you should do valid parenthes
     if right > left:
-        generate(left,right-1,str+')',result)
+        self.generate(left,right-1,str+')',result)
 
 print(generateParenthesis(3))
