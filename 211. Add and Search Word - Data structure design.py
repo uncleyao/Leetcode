f1d7442c77931e86fce8ec9cf29578b55b380aec
Yuan Yao <pengpeng9045@gmail.com>
2017-07-27 22:15:24 -0400

    Create 211. Add and Search Word - Data structure design.py

---
+++ 211. Add and Search Word - Data structure design.py
@@ -0,0 +1,75 @@
+"""
+Design a data structure that supports the following two operations:
+
+void addWord(word)
+bool search(word)
+search(word) can search a literal word or a regular expression string containing only letters a-z or .. A . means it can represent any one letter.
+
+For example:
+
+addWord("bad")
+addWord("dad")
+addWord("mad")
+search("pad") -> false
+search("bad") -> true
+search(".ad") -> true
+search("b..") -> true
+"""
+
+class TrieNode:
+    def __init__(self):
+        self.end = False
+        self.children = {}
+    
+
+class WordDictionary(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+        
+
+    def addWord(self, word):
+        """
+        Adds a word into the data structure.
+        :type word: str
+        :rtype: void
+        """
+        cur = self.root
+        for w in word:
+            if w not in cur.children:
+                cur.children[w] = TrieNode()
+            cur = cur.children[w]
+        cur.end = True
+        
+
+    def search(self, word):
+        """
+        Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+        :type word: str
+        :rtype: bool
+        """
+        return self.__search(word,self.root)
+    
+    def __search(self,word,cur):
+        if not word:
+            return cur.end
+        w = word[0]
+        if w!='.':
+            if w in cur.children:
+                return self.__search(word[1:],cur.children[w])
+            else:
+                return False
+        else:
+            for child in cur.children.values():
+                if self.__search(word[1:],child) == True:
+                    return True
+        return False
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
