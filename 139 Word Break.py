f57b4b9d64b798b88c2dd26c77dc4876cddd3eb8
Yuan Yao <pengpeng9045@gmail.com>
2017-07-28 00:39:24 -0400

    Create 139 Word Break.py

---
+++ 139 Word Break.py
@@ -0,0 +1,40 @@
+class Solution(object):
+##DFS
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        if s in wordDict:
+            return True
+        for k in wordDict:
+            if len(k) <=len(s) and k == s[:len(k)]:
+                if self.wordBreak(s[len(k):],wordDict):
+                    return True
+                else:
+                    continue
+        return False
+        
+####DP
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        dp = [False for i in range(len(s)+1)]
+        dp[0] = True
+        
+        for i in range(len(dp)):
+            if dp[i]:
+                for w in wordDict:
+                    try:
+                        if dp[i+len(w)]:
+                            continue
+                        if s[i:i+len(w)] == w:
+                            dp[i+len(w)] = True
+                    except IndexError:
+                        continue
+        return dp[-1]
+                    
