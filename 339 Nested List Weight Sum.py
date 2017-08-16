b93f1f77d4dc0f79ebd3e2dbccbe4971890fbbcf
Yuan Yao <pengpeng9045@gmail.com>
2017-08-15 23:28:23 -0400

    DFS

---
+++ 339 Nested List Weight Sum.py
@@ -0,0 +1,44 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger(object):
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution(object):
+    def depthSum(self, nestedList):
+        """
+        :type nestedList: List[NestedInteger]
+        :rtype: int
+        """
+        if not nestedList:
+            return 0
+        result = 0
+        stack = [[n,1] for n in nestedList]
+        while stack:
+            next,d = stack.pop(0)
+            if next.isInteger():
+                result += d*next.getInteger()
+            else:
+                for k in next.getList():
+                    stack.append([k,d+1])
+        return result
+        
